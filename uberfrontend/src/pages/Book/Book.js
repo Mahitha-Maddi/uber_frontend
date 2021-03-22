8c8a536aaab2c04df20fb12b9b6f877028ab7c38
Mahitha-Maddi <maddi.s@husky.neu.edu>
2021-03-21 23:10:19 -0400

    Field validation

--- uberfrontend/src/pages/Book/Book.js
+++ uberfrontend/src/pages/Book/Book.js
@@ -4,6 +4,8 @@ import Typography from '@material-ui/core/Typography'
 //import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
+import { FormErrors } from './FormErrors';
+
 
 class Book extends React.Component {
   constructor() {
@@ -13,24 +15,88 @@ class Book extends React.Component {
       destination: '',
       date: '',
       availableBuses: '',
-      user: ''
+      user: '',
+      formErrors: { source: '', destination: '', user: '', date: '' },
+      userValid: false,
+      sourceValid: false,
+      destinationValid: false,
+      dateValid: false,
+      formValid: false,
     }
-    //this._handleSubmit = this._handleSubmit.bind(this);
+
+
+    // this._handleSubmit = this._handleSubmit.bind(this);
   }
 
-  book(item){
+  handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value },
+      () => { this.validateField(name, value) });
+  }
+
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let userValid = this.state.userValid;
+    let sourceValid = this.state.sourceValid;
+    let destinationValid = this.state.destinationValid;
+    let dateValid = this.state.dateValid;
+    let reg = new RegExp(/^[a-z ,.'-]+$/i);
+    let regDate = new RegExp(/(0[1-9]|1[012])[- \/.](0[1-9]|[12][0-9]|3[01])[- \/.](19|20)\d\d/);
+    switch (fieldName) {
+      case 'source':
+        sourceValid = (value.length === 0 || reg.test(value));
+        fieldValidationErrors.source = sourceValid ? '' : ' is invalid or too short';
+        break;
+      case 'destination':
+        destinationValid = (value.length === 0 || reg.test(value)) && this.state.source != this.state.destination
+        fieldValidationErrors.destination = destinationValid ? '' : ' is too short or invalid';
+        break;
+      case 'user':
+        userValid = (value.length === 0 || reg.test(value));
+        fieldValidationErrors.user = userValid ? '' : ' is too short or invalid';
+        break;
+      case 'date':
+        dateValid = value.length === 0 || (!reg.test(value) && regDate.test(value));
+        fieldValidationErrors.date = dateValid ? '' : ' is too short or invalid';
+        break;
+      default:
+        break;
+    }
+    this.setState({
+      formErrors: fieldValidationErrors,
+      sourceValid: sourceValid,
+      destinationValid: destinationValid,
+      userValid: userValid,
+      dateValid: dateValid
+    }, this.validateForm);
+  }
+
+  validateForm() {
+    this.setState({ formValid: this.state.sourceValid && this.state.destinationValid && this.state.userValid && this.state.dateValid });
+  }
+
+  errorClass(error) {
+    return (error.length === 0 ? '' : 'has-error');
+  }
+
+  book(item) {
+
     fetch('http://18.211.239.93:5000/book', {
       method: 'POST', headers: {
         'Content-Type': 'application/json'
-      }, body: JSON.stringify({ source: item.source, destination: item.destination, busnumber: item.busnumber, date: item.date, 
-        startTime: item.startTime, endTime: item.endTime,user: this.state.user})
+      }, body: JSON.stringify({
+        source: item.source, destination: item.destination, busnumber: item.busnumber, date: item.date,
+        startTime: item.startTime, endTime: item.endTime, user: this.state.user
+      })
     })
-      .then(response =>
-        { console.log(response);
-          return response.json();
-        })
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
       .then(data => {
-       alert("Booked successfully! "+data);
+        alert("Booked successfully! " + data);
 
       })
       .catch(error => {
@@ -38,22 +104,27 @@ class Book extends React.Component {
         console.log('Request failed', error)
       });
   }
- 
-  handleSubmit() {
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { source, destination, user, date } = this.state;
     fetch('http://18.211.239.93:5000/checkAvailability', {
       method: 'POST', headers: {
         'Content-Type': 'application/json'
       }, body: JSON.stringify({ source: this.state.source, destination: this.state.destination, date: this.state.date })
     })
-      .then(response =>
-        { console.log(response);
-          return response.json();
-        })
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
       .then(data => {
         console.log(data)
-        this.setState({availableBuses:<tbody>{ data.map((item, index) => (<tr><td key={index}>{item.source}</td><td key={index}>{item.destination}</td><td key={index}>{item.busnumber}</td><td key={index}>{item.date}</td><td key={index}>{item.startTime}</td><td key={index}>{item.endTime}</td>
-          <td><input type="button" value="Book" onClick={this.book.bind(this,item)}/></td></tr>))}</tbody>})
-        console.log('Request successful', data); 
+        this.setState({
+          availableBuses: <tbody>{data.map((item, index) => (<tr><td key={index}>{item.source}</td><td key={index}>{item.destination}</td><td key={index}>{item.busnumber}</td><td key={index}>{item.date}</td><td key={index}>{item.startTime}</td><td key={index}>{item.endTime}</td>
+            <td><input type="button" value="Book" onClick={this.book.bind(this, item)} /></td></tr>))}</tbody>
+        })
+        console.log('Request successful', data);
         //alert(data);
 
       })
@@ -61,106 +132,95 @@ class Book extends React.Component {
         this.setState({ availableBuses: "This is an error page!!" })
         console.log('Request failed', error)
       });
-  }
+    // window.location.reload();
 
+  }
 
   render() {
     const availableBuseslist = this.state.availableBuses;
-    //const array=availableBuseslist.split(':');
     console.log(availableBuseslist.length)
     console.log(availableBuseslist)
-    //console.log(availableBuseslist[0].destination)
-
-    //availableBuseslist.forEach((key) => {console.log(key) });
-    //const j=JSON.parse(availableBuseslist);
-    //console.log(availableBuseslist);
-    //console.log(j[0] +"type:"+j.type);
-   // console.log(JSON.parse(JSON.stringify(availableBuseslist)));
-  
-    
     return (
       <React.Fragment>
         <Paper elevation={6}>
           <div>
-          <br></br><br></br> <br></br><br></br>
+            <br></br><br></br> <br></br><br></br>
             <Typography component="h1" variant="h5">
               {''}
             </Typography>
-            <TextField
-              value={this.state.user}
-              onInput={(e) => {
-                this.setState({ user: e.target.value });
-              }}
-              ref={ref => this.textField = ref}
-              //onKeyUp={this.handleSourceChange.bind(this)}
-              variant="outlined"
-              margin="normal"
-              id="user"
-              label={'UserName'}
-              name="user"
-            /><br></br>
-            <TextField
-              value={this.state.source}
-              onInput={(e) => {
-                this.setState({ source: e.target.value });
-              }}
-              ref={ref => this.textField = ref}
-              //onKeyUp={this.handleSourceChange.bind(this)}
-              variant="outlined"
-              margin="normal"
-              id="source"
-              label={'Source'}
-              name="source"
-            /><br></br>
-            <TextField
-              value={this.state.destination}
-              onInput={(e) => {
-                this.setState({ destination: e.target.value });
-              }}
-              ref={ref => this.textField = ref}
-              //onKeyUp={this.handleDestinationChange.bind(this)}
-              variant="outlined"
-              margin="normal"
-              id="destination"
-              label={'Destination'}
-              name="destination"
-            /><br></br>
-            <TextField
-              value={this.state.date}
-              onInput={(e) => {
-                this.setState({ date: e.target.value });
-              }}
-              ref={ref => this.textField = ref}
-              //onKeyUp={this.handleDateChange.bind(this)}
-              variant="outlined"
-              margin="normal"
-              label={'Date of travel'}
-              name="date"
-            />
+            <form onSubmit={this.handleSubmit}>
+              <div className="panel panel-default">
+                <FormErrors formErrors={this.state.formErrors} />
+              </div>
+              <div className={`form-group ${this.errorClass(this.state.formErrors.user)}`}>
+                <TextField
+                  value={this.state.user}
+                  className="form-control" name="user"
+                  onChange={this.handleUserInput}
+                  type="text"
+                  placeholder="User"
+                  variant="outlined"
+                  margin="normal"
+                /></div><br></br>
+              <div className={`form-group ${this.errorClass(this.state.formErrors.source)}`}>
+                <TextField
+                  className="form-control"
+                  name="source"
+                  value={this.state.source}
+                  onChange={this.handleUserInput}
+                  type="text"
+                  placeholder="Source"
+                  variant="outlined"
+                  margin="normal"
+                /><br /><br /></div>
+              <div className={`form-group ${this.errorClass(this.state.formErrors.destination)}`}>
+                <TextField
+                  name="destination"
+                  className="form-control"
+                  value={this.state.destination}
+                  onChange={this.handleUserInput}
+                  type="text"
+                  placeholder="Destination"
+                  variant="outlined"
+                  margin="normal"
+                /><br /><br /></div>
+              <div className={`form-group ${this.errorClass(this.state.formErrors.date)}`}>
+                <TextField
+                  name="date"
+                  className="form-control"
+                  value={this.state.date}
+                  onChange={this.handleUserInput}
+                  type="text"
+                  placeholder="date"
+                  variant="outlined"
+                  margin="normal"
+                /><br /><br /></div>
+              <input type="submit" value="Check Availability" className="btn btn-primary" onClick={this.handleSubmit.bind(this)} />
+            </form>
+            <br></br>
+            <table border='1'>
+              <thead>
+                <tr>
+                  <th>Source</th>
+                  <th>Destination</th>
+                  <th>Bus Number</th>
+                  <th>Date</th>
+                  <th>Start Time</th>
+                  <th>End Time</th>
+                  <th>Book</th>
+                </tr>
+              </thead>
+              {this.state.availableBuses}
+            </table>
             <br></br><br></br>
-            <input type="button" value="Check Availability" onClick={this.handleSubmit.bind(this)} />
           </div>
-          <br></br>
-         <table border='1'>
-            <thead>
-              <tr>
-                <th>Source</th>
-                <th>Destination</th>
-                <th>Bus Number</th>
-                <th>Date</th>
-                <th>Start Time</th>
-                <th>End Time</th>
-                <th>Book</th>
-              </tr>
-            </thead>
-            {this.state.availableBuses}
-          </table>
-
         </Paper>
-      </React.Fragment>
+      </React.Fragment >
     )
   }
 }
+
+
 
 
 export default Book
